1dba7fbd5437d3f1b1fc6c50577677114492cfde
Ivan Bakaidov <ibakaidov@ya.ru>
2023-03-27 12:22:41 +0300

    fix version script

--- version.js
+++ version.js
@@ -8,6 +8,6 @@ const version = content.match(/bundleVersion:.(.*)/)[1];
 
 const installer = fs.readFileSync(".\\installer.iss", 'utf-8').replace("$version", version)
 fs.writeFileSync(".\\installer.iss", 'utf-8')
-fs.writeFileSync('".\\build\\linka.plays\\version.json', JSON.stringify({
+fs.writeFileSync('.\\build\\linka.plays\\version.json', JSON.stringify({
     version
 }))
