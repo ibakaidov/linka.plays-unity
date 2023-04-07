b09bf5054535cda6e0c8becfebb7170a0be81cc2
Ivan Bakaidov <ibakaidov@ya.ru>
2023-04-07 20:39:36 +0300

    Update version.js

--- version.js
+++ version.js
@@ -1,11 +1,11 @@
 const fs = require('fs')
 
-const content = fs.readFileSync(".\\ProjectSettings\\ProjectSettings.asset", 'utf-8')
+const content = fs.readFileSync("./ProjectSettings/ProjectSettings.asset", 'utf-8')
 
 const version = content.match(/bundleVersion:.(.*)/)[1];
 
-const installer = fs.readFileSync(".\\installer.iss", 'utf-8').replace("$version", version)
-fs.writeFileSync(".\\installer.iss", installer)
-fs.writeFileSync('.\\build\\linka.plays\\version.json', JSON.stringify({
+const installer = fs.readFileSync("./installer.iss", 'utf-8').replace("$version", version)
+fs.writeFileSync("./installer.iss", installer)
+fs.writeFileSync('./build/linka.plays/version.json', JSON.stringify({
     version
 }))
